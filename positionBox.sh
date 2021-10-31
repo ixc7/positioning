10605083b501b158dc7a15d56aefdf419d55fde4
Your name here <isaacxpreston7@gmail.com>
2021-10-30 20:24:19 -0400

    functions to make boxes

--- positionBox.sh
+++ positionBox.sh
@@ -1,1 +1,31 @@
 #!/usr/local/bin/bash
+
+source utils.sh
+
+termW=$(tput cols)
+termH=$(tput lines)
+
+boxW=10
+boxH=5
+
+startX=12
+startY=15
+
+endX=$(( $startX + $boxW ))
+endY=$(( $startY + $boxH ))
+
+charX="="
+charY="|"
+
+outerCol=$(repeatStr $charX $boxW)
+innerCol=$charY$(repeatStr " " $(( $boxW - 2 )))$charY
+innerCols=$(repeatStr "${innerCol}\n" $(( $boxH - 2 )))
+
+boxOutline="
+$outerCol
+$innerCols
+$outerCol
+"
+
+echo "$boxOutline"
+
